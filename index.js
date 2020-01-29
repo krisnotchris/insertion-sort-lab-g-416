6cce3c7643e40a6f5d042f9c05e493a0f31e0c2b
Kris Henderson <krishenderson@gmail.com>
2020-01-29 02:57:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function findMinAndRemove(array){
-
+  let min = array[0]
+  for(var i = 0; i < array.length; i++) {
+    if (array[i] < min ) {
+      min = array[i]
+    }
+  }
+  array.splice(array.indexOf(min), 1)
+  return min;
 }
 
 function insertionSort(array){
-
+  findMinAndRemove(array);
 }
